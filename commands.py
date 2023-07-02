7b98f2ac204909b541fc3afcb4ac4cd7b4340f43
mariyabykova <mariab@bk.ru>
2023-07-02 21:04:32 +0300

    Создана функция, возвращающая ответ chatgpt на вопрос пользователя

--- commands.py
+++ commands.py
@@ -1,9 +1,23 @@
 import openai
+import os
 from telegram import Update
 from telegram.ext import CallbackContext
 
 
+MODEL = 'text-davinci-003'
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
 async def start(update: Update, context: CallbackContext):
     if update.message:
-        await update.message.reply_text('Привет! Я твой бот-помощник! '
-                                        'Задавай мне любые вопросы!')
+        await update.message.reply_text('Привет! Я твой бот-помощник!'
+                                        ' Задавай мне любые вопросы!')
+
+
+async def get_answer_from_chatgpt(update: Update, context: CallbackContext):
+    response = openai.Completion.create(
+        model=MODEL,
+        prompt=update.message.text,
+        max_tokens=2000,
+    )
+    return await update.message.reply_text(response.choices[0].text)
--- simple_chatgpt_bot.py
+++ simple_chatgpt_bot.py
@@ -2,15 +2,18 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler
+import openai
+from telegram.ext import (ApplicationBuilder, CommandHandler,
+                          filters, MessageHandler)
 
-from commands import start
+from commands import start, get_answer_from_chatgpt
 
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-OPENAI_API_TOKEN = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,6 +23,10 @@ logging.basicConfig(
 def main():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        get_answer_from_chatgpt,
+    ))
     application.run_polling()
 
 
