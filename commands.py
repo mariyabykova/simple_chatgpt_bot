98a0486dfcbb2e7a89dfafb522fa72b540177e37
mariyabykova <mariab@bk.ru>
2023-07-04 14:04:12 +0300

    Исправлен файл commands

--- commands.py
+++ commands.py
@@ -13,15 +13,14 @@ AUTHORIZATION_ERROR_MESSAGE = ('Вы не имеете права пользов
                                ' Выполните команду /start,'
                                ' а затем введите пароль.')
 ALLOWED_VISITORS = []
+CHAT_OBJECT = {
+    'role': 'system',
+    'content': 'You are a programming assistant'
+}
+CHAT_HISTORY = [CHAT_OBJECT]
 MODEL = 'gpt-3.5-turbo'
 PASSWORD = 0
 SECRET_PASSWORD = os.getenv('SECRET_PASSWORD')
-CHAT_HISTORY = [
-    {
-        'role': 'system',
-        'content': 'You are a programming assistant',
-    }
-]
 SUM_TOKENS = 0
 TOTAL_TOKENS = 4000
 
@@ -34,10 +33,7 @@ def update_history(message, role, content):
 def reset_messages():
     """Вспомогательная функция для очистки истории чата."""
     CHAT_HISTORY.clear()
-    CHAT_HISTORY.append({
-        'role': 'system',
-        'content': 'You are a programming assistant'
-    })
+    CHAT_HISTORY.append(CHAT_OBJECT)
 
 
 def check_users(user_id):
@@ -71,7 +67,10 @@ async def enter_password(update: Update, context: CallbackContext):
     if password == SECRET_PASSWORD:
         ALLOWED_VISITORS.append(update.message.chat.id)
         await update.message.reply_text(
-            'Пароль верный. Можете начинать работу.'
+            'Пароль верный. Я готов к работе с тобой!'
+            ' Можешь задавать мне любые вопросы.'
+            ' Очистить историю поиска можно командой /reset.'
+            ' Посмотреть остаток токенов после запроса можно командой /tokens'
         )
         return ConversationHandler.END
     else:
@@ -110,6 +109,7 @@ async def get_answer_from_chatgpt(update: Update, context: CallbackContext):
                 messages=CHAT_HISTORY,
                 max_tokens=TOTAL_TOKENS,
             )
+            print(CHAT_HISTORY)
             SUM_TOKENS += response['usage']['total_tokens']
             if SUM_TOKENS >= TOTAL_TOKENS:
                 await update.message.reply_text('Вы использовали все токены!'
