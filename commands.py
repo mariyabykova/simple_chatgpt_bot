a0fea9b4a7d0496a725f56a7d4ccd3cfe83f265b
mariyabykova <mariab@bk.ru>
2023-07-04 19:42:43 +0300

    Добавлена функция для подсчета токенов в тексте запроса

--- commands.py
+++ commands.py
@@ -1,12 +1,21 @@
+import logging
 import os
+import sys
 
 from dotenv import load_dotenv
 import openai
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import CallbackContext, ConversationHandler
+import tiktoken
 
 
 load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 AUTHORIZATION_ERROR_MESSAGE = ('Вы не имеете права пользоваться ботом.'
@@ -15,15 +24,22 @@ AUTHORIZATION_ERROR_MESSAGE = ('Вы не имеете права пользов
 ALLOWED_VISITORS = []
 CHAT_OBJECT = {
     'role': 'system',
-    'content': 'You are a programming assistant'
+    'content': 'You are a bot-assistant'
 }
 CHAT_HISTORY = [CHAT_OBJECT]
 MODEL = 'gpt-3.5-turbo'
 PASSWORD = 0
 SECRET_PASSWORD = os.getenv('SECRET_PASSWORD')
 SUM_TOKENS = 0
-TOTAL_TOKENS = 4000
+TOTAL_TOKENS = 3500
 
+
+def count_num_tokes(string, encoding_name):
+    """Вспомогательная функция для подсчёта токенов в тексте,
+    который вводит пользователь."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
 
 def update_history(message, role, content):
     """Вспомогательная функция для обновления истории чата."""
@@ -80,7 +96,7 @@ async def enter_password(update: Update, context: CallbackContext):
 
 
 async def reset(update: Update, context: CallbackContext):
-    """Очистка истории чата."""
+    """Очистка истории чата. Команда /reset."""
     if check_users(update.message.chat.id):
         reset_messages()
         await update.message.reply_text('История чата была очищена.')
@@ -89,7 +105,7 @@ async def reset(update: Update, context: CallbackContext):
 
 
 async def count_tokens(update: Update, context: CallbackContext):
-    """Подсчёт количества оставшихся токенов."""
+    """Подсчёт количества оставшихся токенов. Команда /tokens"""
     if check_users(update.message.chat.id):
         await update.message.reply_text(
             f'Ваш остаток токенов: {TOTAL_TOKENS - SUM_TOKENS}',
@@ -103,14 +119,21 @@ async def get_answer_from_chatgpt(update: Update, context: CallbackContext):
     if check_users(update.message.chat.id):
         try:
             global SUM_TOKENS
+            if count_num_tokes(update.message.text, 'cl100k_base') >= 500:
+                return await update.message.reply_text(
+                    'Вы использовали слишком много токенов.'
+                    ' Сократите запрос.'
+                )
             update_history(CHAT_HISTORY, 'user', update.message.text)
             response = openai.ChatCompletion.create(
                 model=MODEL,
                 messages=CHAT_HISTORY,
                 max_tokens=TOTAL_TOKENS,
             )
-            update_history(CHAT_HISTORY, 'assistant', response.choices[0].message.content)
+            update_history(CHAT_HISTORY, 'assistant',
+                           response.choices[0].message.content)
             print(CHAT_HISTORY)
+            print(response['usage']['prompt_tokens'])
             SUM_TOKENS += response['usage']['total_tokens']
             if SUM_TOKENS >= TOTAL_TOKENS:
                 await update.message.reply_text('Вы использовали все токены!'
@@ -119,7 +142,8 @@ async def get_answer_from_chatgpt(update: Update, context: CallbackContext):
             return await update.message.reply_text(
                 response.choices[0].message.content
             )
-        except Exception:
+        except Exception as error:
+            logger.error(f'{error}')
             reset_messages()
             await update.message.reply_text('Ошибка! История чата будет очищена!'
                                             ' Попробуйте повторить запрос.')
